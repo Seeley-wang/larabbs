1e496c6ef9a896c5199b8e6ed4255257bda7afca
clown <wangyw.zt@gmail.com>
2018-07-10 02:27:54 +0800

    basic user profile editing

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,7 +1,7 @@
 <?php
 
 function route_class()
-{
+{	
 	// 将当前请求的路由名称转化为css类名
-	return str_replace('.', '-', Route::curentRouteName());
+	return str_replace('.', '-', Route::currentRouteName());
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,27 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=drivce-width,initial-scale=1">
-	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>@yield('title','laraBBS')</title>
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
-<div id="app" class="{{ route_class() }}-page">
+    <div id="app" class="{{ route_class() }}-page">
 
-	@include('layouts._header')
+        @include('layouts._header')
 
-	<div class="container">
+        <div class="container">
+            @include('layouts._message')
+            @yield('content')
 
-		@yield('content')
+        </div>
 
-	</div>
-	
-	@include('layouts._footer')
-</div>
+        @include('layouts._footer')
+    </div>
 
-<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
