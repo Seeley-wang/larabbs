9a69336085a2502c2abbcf68c8081e5f85118f9b
clown <wangyw.zt@gmail.com>
2018-07-09 22:05:15 +0800

    finish basic page layout

---
+++ bootstrap/helpers.php
@@ -0,0 +1,7 @@
+<?php
+
+function route_class()
+{
+	// 将当前请求的路由名称转化为css类名
+	return str_replace('.', '-', Route::curentRouteName());
+}
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title','首页')
+@section('content')
+<h1>首页s</h1>
+@stop
