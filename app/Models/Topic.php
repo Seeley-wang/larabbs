ad461b504079708eaa5fe9faabc70e536dc721df
clown <wangyw.zt@gmail.com>
2018-07-11 03:44:18 +0800

    Seeding topics and users data

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,6 +2,13 @@
 
 namespace App\Models;
 
+/**
+ * App\Models\Topic
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model ordered()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model recent()
+ * @mixin \Eloquent
+ */
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
