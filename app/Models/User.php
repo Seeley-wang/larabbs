410f2e86ea6e108a8feb5795687e1c5085815584
clown <wangyw.zt@gmail.com>
2018-07-12 22:55:30 +0800

    Notify author when topic replied

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,7 +31,19 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
  */
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+
+    public function notify($instance)
+    {
+        if ($this->id == \Auth::id()) {
+            return;
+        }
+
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -39,7 +51,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','avatar','introduction',
+        'name', 'email', 'password', 'avatar', 'introduction',
     ];
 
     /**
@@ -55,8 +67,16 @@ class User extends Authenticatable
     {
         return $this->hasMany(Topic::class);
     }
+
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -36,6 +36,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'clown';
         $user->email = 'clown@clown.com';
+        $user->password = bcrypt('password');
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
         $user->save();
     }
