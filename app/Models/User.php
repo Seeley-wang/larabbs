e8e5de47cd3aa6a077666c2e84fac9bb8d6bf09d
clown <wangyw.zt@gmail.com>
2018-07-12 14:16:02 +0800

    List user replies

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Topic::class);
     }
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
---
+++ resources/views/topics/_reply_box.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Clown
+ * Date: 18/7/12
+ * Time: 下午2:02
+ */
